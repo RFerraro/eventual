04451c94cceac009872f3c8849c4a2163ea77e88
Ryan Ferraro <ferraro.ryan.m@gmail.com>
2016-02-06 16:30:47 -0800

    Configured continuous integration builds with Appveyor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eventual
 A futures library with continuations, inspired by the upcoming C++ Extensions for Concurrency (ISO/IEC TS 19571:xxxx)
+
+| Compiler | Platforms | CI Status |
+| --- | --- | --- |
+| Visual Studio 2015 | Windows (x86/x86-64) | [![Build status](https://ci.appveyor.com/api/projects/status/github/RFerraro/eventual?branch=CI&svg=true)](https://ci.appveyor.com/project/RFerraro/eventual/branch/CI) |
+
---
+++ appveyor/install_conan.ps1
@@ -0,0 +1,26 @@
+﻿$conanUrl = "http://downloads.conan.io/latest_windows"
+$conanInstallerPath = [System.IO.Path]::Combine($Env:USERPROFILE, "Downloads", "conan_installer.exe")
+$conanInstallPath = [System.IO.Path]::Combine(${Env:ProgramFiles(x86)}, "Conan", "conan")
+$conanExe = [System.IO.Path]::Combine($conanInstallPath, "conan.exe")
+
+if(-Not (Test-Path $conanInstallerPath))
+{
+    Write-Host -ForegroundColor White ("Downloading Conan from: " + $conanUrl)
+    Start-FileDownload $conanUrl -FileName $conanInstallerPath
+}
+
+Write-Host -ForegroundColor White "Installing Conan..."
+
+# Pipe the output so powershell waits for completion
+(& $conanInstallerPath /VERYSILENT) | Out-Null
+
+$env:Path="$env:Path;$conanInstallPath"
+
+while(-Not (Test-Path $conanExe))
+{
+    Start-Sleep -s 1
+}
+
+Write-Host -ForegroundColor White "Conan Installed"
+
+(& $conanExe --version) | Write-Host -ForegroundColor White
