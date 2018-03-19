89e26efc0bee9914f1125e4ccbf16c05701c7b96
Ryan Ferraro <ferraro.ryan.m@gmail.com>
2018-03-18 17:34:32 -0700

    Update outdated configuration files used for CIT.
    * Updated conan package source to use the bincrafters GTest package.
    * Pinned codecov tool to a version that currently works on AppVeyor/Windows.

--- tools/docker/build-project.sh
+++ tools/docker/build-project.sh
@@ -35,48 +35,6 @@ function usage
     echo "    CI                defines default behavior for code coverage analysis."
 }
 
-function write_conan_settings
-{
-# based on default settings from conan 0.26.1
-cat > ~/.conan/settings.yml << EOF
-
-os:
-    Windows:
-    Linux:
-    Macos:
-    Android:
-        api_level: ANY
-    iOS:
-        version: ["7.0", "7.1", "8.0", "8.1", "8.2", "8.3", "9.0", "9.1", "9.2", "9.3", "10.0", "10.1", "10.2", "10.3"]
-    FreeBSD:
-    SunOS:
-    Arduino:
-        board: ANY
-arch: [x86, x86_64, ppc64le, ppc64, armv6, armv7, armv7hf, armv8, sparc, sparcv9, mips, mips64, avr]
-compiler:
-    sun-cc:
-        version: ["5.10", "5.11", "5.12", "5.13", "5.14"]
-        threads: [None, posix]
-        libcxx: [libCstd, libstdcxx, libstlport, libstdc++]
-    gcc:
-        version: ["4.1", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.1", "5.2", "5.3", "5.4", "6.1", "6.2", "6.3", "6.4", "7.1", "7.2"]
-        libcxx: [libstdc++, libstdc++11]
-        threads: [None, posix, win32] #  Windows MinGW
-        exception: [None, dwarf2, sjlj, seh] # Windows MinGW
-    Visual Studio:
-        runtime: [MD, MT, MTd, MDd]
-        version: ["8", "9", "10", "11", "12", "14", "15"]
-    clang:
-        version: ["3.3", "3.4", "3.5", "3.6", "3.7", "3.8", "3.9", "4.0", "5.0"]
-        libcxx: [libstdc++, libstdc++11, libc++]
-    apple-clang:
-        version: ["5.0", "5.1", "6.0", "6.1", "7.0", "7.3", "8.0", "8.1", "9.0"]
-        libcxx: [libstdc++, libc++]
-build_type: [None, Debug, Release]
-
-EOF
-}
-
 function install_conan
 {
     echo "Installing latest Conan C/C++ package manager."
@@ -85,9 +43,6 @@ function install_conan
     
     echo "Configuring bincrafters source for Conan."
     conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-    
-    #echo "Configuring custom settings for Conan."
-    #write_conan_settings
 
     # Update conan data directory for caching by the CI environment.
     echo "Setting conan data path to: /tmp/conan"
